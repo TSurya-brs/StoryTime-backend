4c4beed38baeb92a06f0f356b191e87a52e214a2
manohar.v@nyros.com <manohar.v@nyros.com>
2024-12-11 10:57:07 +0530

    Removing Files

--- src/routes/categoryRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { checkToken } from "../middleware/authMiddleware.js";
-
-import { getCategories } from "../controllers/categoryController.js";
-
-const router = express.Router();
-router.get("/", checkToken, getCategories);
-
-export default router;
--- src/routes/languageRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getLanguages } from "../controllers/languageController.js";
-
-const router = express.Router();
-
-router.get("/", getLanguages);
-
-export default router; // Export the router to use it in other files
